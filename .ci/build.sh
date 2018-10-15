7e422484ec53d13e7a7c5b214b60cbd83d35cc8a
Leon Schoorl <leon@qbaylogic.com>
2018-10-15 11:54:06 +0200

    Limit concurrency

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,4 +2,6 @@
 set -xeo pipefail
 git submodule update --init --depth 1
 cabal new-update
+# workaround GHC bug 9221, see https://github.com/haskell-CI/haskell-ci/blob/f67bc41621d40d6559684be5406d65409df4c480/README.md#known-issues
+sed -i 's/^jobs:/-- jobs:/' ${HOME}/.cabal/config
 cabal new-build all
