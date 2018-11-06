1f423ee65c416931cc8cc7a732954ac1eeb2440f
Leon Schoorl <leon@qbaylogic.com>
2018-11-06 17:52:03 +0100

    wobble cache

--- .ci/build.sh
+++ .ci/build.sh
@@ -4,4 +4,5 @@ git submodule update --init --depth 1
 cabal new-update
 # workaround GHC bug 9221, see https://github.com/haskell-CI/haskell-ci/blob/f67bc41621d40d6559684be5406d65409df4c480/README.md#known-issues
 sed -i 's/^jobs:/-- jobs:/' ${HOME}/.cabal/config
+echo "store-dir: ${PWD}/cabal-store" >> ${HOME}/.cabal/config
 cabal new-build all
--- .ci/test.sh
+++ .ci/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xeo pipefail
-cabal new-test all
+cabal new-test --enable-tests clash-cosim clash-prelude
 cabal new-run -- clash-testsuite -j16
