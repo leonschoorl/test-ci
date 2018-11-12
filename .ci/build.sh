a798417e85f5b759051e42b7bc4996629df9804c
Leon Schoorl <leon@qbaylogic.com>
2018-11-12 15:18:12 +0100

    wobble gitlab ci stuff

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -xeo pipefail
-git submodule update --init --depth 1
-cabal new-update
 # workaround GHC bug 9221, see https://github.com/haskell-CI/haskell-ci/blob/f67bc41621d40d6559684be5406d65409df4c480/README.md#known-issues
 sed -i 's/^jobs:/-- jobs:/' ${HOME}/.cabal/config
 echo "store-dir: ${PWD}/cabal-store" >> ${HOME}/.cabal/config
+cabal new-update
 cabal new-build all
+cabal new-build $(ghc-pkg list --global --simple-output --names-only | sed 's/\([a-zA-Z0-9-]\{1,\}\) */--constraint="\1 installed" /g') all | sh
--- .ci/setup.sh
+++ .ci/setup.sh
@@ -4,10 +4,4 @@ apt-get update -q
 apt-get install -yq cabal-install-head $GHC
 cabal --version
 ghc --version
-cat > cabal.project.local << END_CABAL_PROJECT_LOCAL
-package clash-testsuite
-  flags: travisci
-package *
-  documentation: False
-END_CABAL_PROJECT_LOCAL
-cat cabal.project.local
+cp .ci/project.cabal.local .
