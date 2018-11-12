d6cf5c7fe6b4640ed092a3abdca5fb2cc769fcf8
Leon Schoorl <leon@qbaylogic.com>
2018-11-12 15:44:09 +0100

    wobble

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -xeo pipefail
+# run new-update first to generate the cabal config file that we can then modify
+cabal new-update
 # workaround GHC bug 9221, see https://github.com/haskell-CI/haskell-ci/blob/f67bc41621d40d6559684be5406d65409df4c480/README.md#known-issues
 sed -i 's/^jobs:/-- jobs:/' ${HOME}/.cabal/config
 echo "store-dir: ${PWD}/cabal-store" >> ${HOME}/.cabal/config
-cabal new-update
 cabal new-build all
 cabal new-build $(ghc-pkg list --global --simple-output --names-only | sed 's/\([a-zA-Z0-9-]\{1,\}\) */--constraint="\1 installed" /g') all | sh
